11612022fe04fcf525deb560bcbc5fd04bc9707b
Wanjiku Kariuki <c.wanjikukariuki@gmail.com>
2023-02-09 10:50:59 +0300

    lesson 1

---
+++ condition.py
@@ -0,0 +1,14 @@
+num=eval(input("enter a number:"))
+
+if num<0:
+    print("wrong input")
+else:
+    if num > 50:
+        x = num/2
+        print(x)
+    else:
+        print("Number is less than 50")
+
+
+
+
